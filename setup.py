39cbb7fd261e2374c0f14dcdd16ad5324a100252
Nathan Merrill <mathiscool3000@gmail.com>
2020-02-04 11:49:52 +0000

    Update github url

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A python library for printing pretty status messages",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/nathansmerrill/sticons",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
