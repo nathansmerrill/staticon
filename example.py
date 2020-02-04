26c71b5b91479acd00c096a86f490f1fe4f21a67
Nathan Merrill <mathiscool3000@gmail.com>
2020-02-04 11:46:51 +0000

    Add example.py

---
+++ example.py
@@ -0,0 +1,7 @@
+from sticons import sprint
+
+sprint('info', 'This is info')
+sprint('success', 'This is success')
+sprint('warning', 'This is warning')
+sprint('error', 'This is error')
+sprint('critical', 'This is critical')
